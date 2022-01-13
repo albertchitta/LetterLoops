c9da6c22a9b35f9d9c59ba196cb9709920da9cb0
albertchitta <albert.w.chittaphong@gmail.com>
2022-01-12 21:46:59 -0600

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter random letters (Ex. abcd):");
+            string input = Console.ReadLine();
+            char[] array = input.ToCharArray();
+            int count = 1;
+
+            foreach (char c in input)
+            {
+                for (int i = 0; i < Array.IndexOf(array, c) + 1; i++)
+                {
+                    if (i == 0)
+                    {
+                        Console.Write(Char.ToUpper(c));
+                    }
+                    else
+                    {
+                        Console.Write(Char.ToLower(c));
+                    }
+                }
+
+                count++;
+
+                if (count <= input.Length)
+                {
+                    Console.Write("-");
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
